ed80a669ed6b1cac754a9f62ce7748eaf4917814
Yoseph Kim <yosephmk@uci.edu>
2026-05-23 18:31:32 -0700

    changes to frotnier and actions

--- src/MyAI.py
+++ src/MyAI.py
@@ -46,7 +46,6 @@ class MyAI( AI ):
         self._move_y = startY
 
         self._flags = set()
-        self._pending_actions = set() 
         self._frontier = []
         
         self._remaining_unmarked_cells = [] # empty until self._total_mines == len(self._flags)
@@ -67,63 +66,47 @@ class MyAI( AI ):
         - 0 indexed board (dimensions excluded)
         - board always starts with a '0' tile
         '''
-        print(self._pending_actions)
-        print(self._frontier)
         if self._all_mines_identified:
-        #put a flag if mine is identified.
-        #put a neighbors of identified mine.
-            if self._remaining_unmarked_cells:
-                return self._clearBoard()
-
-        self._updateBoard(number) #updates effective label according to last action (flag --> update neighbors, uncover --> update self)
-        self._updateFrontier(number)
+            return self._clearBoard()
+        # updates effective label according to last action (flag --> update neighbors, uncover --> update self)
 
         # UPDATE EFFECTIVE LABEL OF POPPED self._move_x, self._move_y HERE BASED ON NUMBER OF NEARBY FLAGS.
         # **** doesn't work (effective label should already be represented on the board)
         #effective label == 0
+        self._updateBoard(number)
+        self._updateFrontier(number)
+        print(self._frontier)
+        print("x:", self._move_x, "y:", self._move_y, "tile_no",self._board[self._move_x][self._move_y])
+
         returning_action = None
-        while returning_action == None: # ensure we always return an action
-            unmarked_neighbors = [] 
-            if not self._frontier:
-                self._move_x, self._move_y = self._getRandomMove()
-                returning_action = Action(AI.Action.UNCOVER, self._move_x, self._move_y)
-                break
+        checked_tiles= set()
+        
+        while self._frontier and returning_action == None: # ensure we always return an action
+            tile_no, move_x, move_y = heapq.heappop(self._frontier)
+            unmarked_neighbors = self._getUnMarkedNeighbors(move_x, move_y)
 
-            while not unmarked_neighbors and self._frontier:
-                tile_no, self._move_x, self._move_y = heapq.heappop(self._frontier)
-                unmarked_neighbors = self._getUnMarkedNeighbors(self._move_x, self._move_y)
+            # effective label == 0
+            if self._getEffectiveLabel(move_x, move_y) == 0 and unmarked_neighbors: # all cells are safe
+                returning_action = self._makeMove(AI.Action.UNCOVER, move_x, move_y, unmarked_neighbors)
+         #effective label == len(UnMarkedNeighbors)
+            elif self._getEffectiveLabel(move_x, move_y) == len(unmarked_neighbors) and unmarked_neighbors:
+                returning_action = self._makeMove(AI.Action.FLAG, move_x, move_y, unmarked_neighbors)
+            else:
+                checked_tiles.add((move_x, move_y))
 
-            if self._getEffectiveLabel(self._move_x, self._move_y) == 0:
-                #all cells are safe.
-                for x,y in unmarked_neighbors:
-                    self._pending_actions.add((x,y, AI.Action.UNCOVER))
-
-        #effective label == len(UnMarkedNeighbors)
-            elif self._board[self._move_x][self._move_y] == len(unmarked_neighbors):
-                for mine_x, mine_y in unmarked_neighbors:
-                    self._flags.add((mine_x, mine_y))
-                    self._pending_actions.add((mine_x, mine_y, AI.Action.FLAG))
-                    neighbors_of_mine = self._getUnMarkedNeighbors(mine_x, mine_y)
-                    for neighbor_x, neighbor_y in neighbors_of_mine:
-                        heapq.heappush(self._frontier, (self._getFrontierPriority(neighbor_x, neighbor_y), neighbor_x, neighbor_y))
-                        # something to note: --> potential duplicates within the priqueue. May or may not be an issue?
-
-        if len(self._flags) == self._total_mines:
+        if len(self._flags) == self._total_mines: # check if board is finished
             #all mines identified
             self._remaining_unmarked_cells = self._getAllUnMarkedCell()
             self._all_mines_identified = True
         
-        if self._pending_actions:
-            move_x, move_y, ai_action = self._pending_actions.pop()
-            returning_action = Action(ai_action, move_x, move_y)
-        '''
-        safe_guess = self._getSafeGuess()
-        if safe_guess:
-            self._move_x = safe_guess[0]
-            self._move_y = safe_guess[1]
-            return Action(AI.Action.UNCOVER, self._move_x, self._move_y)
-        '''
-        #RETURN RANDOM ACTION HERE
+        if not self._frontier and returning_action == None: # check if we failed to get a playable move (have to use random moves)
+            self._move_x, self._move_y = self._getSafeGuess()
+            returning_action = Action(AI.Action.UNCOVER, self._move_x, self._move_y)
+    
+        while checked_tiles: # push back all moves into frontie
+            move_x, move_y = checked_tiles.pop()
+            heapq.heappush(self._frontier, (self._getEffectiveLabel(move_x, move_y), move_x, move_y))
+
         return returning_action
 
         ########################################################################
@@ -140,19 +123,21 @@ class MyAI( AI ):
             return Action(AI.Action.UNCOVER, self._move_x, self._move_y)
         return Action(AI.Action.LEAVE)
     
-
     def _updateBoard(self, number):
         self._board[self._move_x][self._move_y] = number
-    
-    def _updateFrontier(self,number):
-        if number == -1:
-            # add uncovered neighbors into the frontier (potential options available)
-            neighbors = self._getNumberedNeighbors(self._move_x, self._move_y)
-            for n_x,n_y in neighbors:
-                heapq.heappush(self._frontier, (self._getEffectiveLabel(n_x, n_y), n_x ,n_y))
-        else:
-            heapq.heappush(self._frontier, (self._getEffectiveLabel(self._move_x,self._move_y), self._move_x, self._move_y))
-    
+
+    def _updateFrontier(self, number):
+        if number != -1:
+            heapq.heappush(self._frontier, (self._getEffectiveLabel(self._move_x, self._move_y), self._move_x, self._move_y))
+
+    def _makeMove(self, ai_action, move_x, move_y, unmarked_neighbors):
+        print("parent_move: ", move_x, move_y)
+        print(unmarked_neighbors)
+        if len(unmarked_neighbors) > 1: # if multiple actions available
+            heapq.heappush(self._frontier, (self._getEffectiveLabel(move_x, move_y), move_x, move_y))
+        self._move_x, self._move_y = unmarked_neighbors[0]
+        return Action(ai_action, self._move_x, self._move_y)
+
     def _getEffectiveLabel(self, move_x, move_y):
         marked_neighbors = self._getMarkedNeighbors(move_x, move_y)
         return self._board[move_x][move_y] - len(marked_neighbors)
@@ -185,7 +170,7 @@ class MyAI( AI ):
                         candidate_risk[(ux, uy)] = max(candidate_risk[(ux, uy)], risk)
 
         if not candidate_risk:
-            return None
+            return self._getRandomMove()
 
         return min(candidate_risk, key=candidate_risk.get)
         
@@ -264,3 +249,4 @@ class MyAI( AI ):
                 neighbors.append((move_x + 1, move_y - 1))
 
         return neighbors
+    
